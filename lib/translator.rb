40f0bc3c0d5e6d44757f97ef184f5b285c184ef3
Eugenia Kim <elk303@nyu.edu>
2019-08-08 04:17:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,9 +15,10 @@ def get_japanese_emoticon(file_path, english_emoticon)
   japanese_emoticon = load_library(file_path)["get_emoticon"][english_emoticon]
     if japanese_emoticon
       return japanese_emoticon
-      "Sorry, that emoticon was not found"
+    else
+      return "Sorry, that emoticon was not found"
     end
-  emojis
+  end
 end
 
 
