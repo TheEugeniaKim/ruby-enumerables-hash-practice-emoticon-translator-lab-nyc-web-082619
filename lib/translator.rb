ae3dea6286b4d75724b742f76b9b4d9a1761ef38
Eugenia Kim <elk303@nyu.edu>
2019-08-08 03:48:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
     emojis["get_emoticon"].each do |english_emoji, japanese_emoji|
       if english_emoticon = english_emoji
         return japanese_emoji
-      end
-      apology
+      else apology
     end
   end
 
