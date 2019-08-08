3888be060cb711a51007186cf8b93aaa7d53d487
Eugenia Kim <elk303@nyu.edu>
2019-08-08 04:23:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
 
 
 def get_english_meaning(file_path, emoticon)
-  meaning == load_library(file_path)["get_meaning"][emoticon]
+  meaning = load_library(file_path)["get_meaning"][emoticon]
   if meaning
     return meaning
   else
