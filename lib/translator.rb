dd9bc75832bc97634e7bd8b689aa920f7934e018
Eugenia Kim <elk303@nyu.edu>
2019-08-08 03:48:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ end
 
 
 def get_japanese_emoticon(file_path, english_emoticon)
-  apology = "Sorry, that emoticon was not found"
+  "Sorry, that emoticon was not found"
   emojis = load_library(file_path)
     emojis["get_emoticon"].each do |english_emoji, japanese_emoji|
       if english_emoticon = english_emoji
