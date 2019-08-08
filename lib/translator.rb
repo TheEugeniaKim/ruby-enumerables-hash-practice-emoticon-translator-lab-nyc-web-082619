0d9b79af7b2a0bbe72bfc1bc13ae2ca78a9932a8
Eugenia Kim <elk303@nyu.edu>
2019-08-08 04:25:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_english_meaning(file_path, emoticon)
   if meaning
     return meaning
   else
-    return "Sorry, that emotion was not found"
+     return "Sorry, that emoticon was not found"
  end
 end
 
