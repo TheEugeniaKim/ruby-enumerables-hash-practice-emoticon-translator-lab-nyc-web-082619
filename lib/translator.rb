5f946090fffa827e945cce0a658ee69a38ae6b46
Eugenia Kim <elk303@nyu.edu>
2019-08-08 02:12:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   file_path = 'lib/emoticons.yml'
-  library = load_library(file_path)
+  library = def load_library(file_path)
     if library
       return library["get_japanese_emoticon"]
     end
